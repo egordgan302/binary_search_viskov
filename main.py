70e9c1ff913ec6383f4b121fe7010bac65225304
egordgan302 <143101848+egordgan302@users.noreply.github.com>
2023-12-11 09:29:09 +0300

    Update main.py binary

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 #напишите программу для бинарного поиска загаданного числа
-
-def binary_search():
-    pass
+def binary_search(min_num, max_num):
+    user_num = int(input("Введите целое число:"))
+    center = (min_num+max_num)//2
+    while True:
+        print(f"Ваше число {center}?")
+        check = input("Если больше > , если меньше < , если равно =")
+        
+        if check == ">":
+            min_num = center
+            center = (min_num+max_num)//2
+        elif check == "<":
+            max_num = center
+            center = (min_num+max_num)//2
+        elif check == "=":
+            print(f"Я угадал!")
+            break
+        else:
+            print("Я не знаю это число")
+            break
+binary_search(int(input("Минимальное число:")), int(input("Максимальное число:")))    
+             
